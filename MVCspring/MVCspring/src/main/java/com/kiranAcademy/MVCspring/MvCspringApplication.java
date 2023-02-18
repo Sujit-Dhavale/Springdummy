2669a558a4a3c2c7c87b098122a2d8de62716ad2
Sujit-Dhavale <sdhavale538@gmail.com>
2023-02-18 10:25:08 +0530

    demo

---
+++ MVCspring/MVCspring/src/main/java/com/kiranAcademy/MVCspring/MvCspringApplication.java
@@ -0,0 +1,13 @@
+package com.kiranAcademy.MVCspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvCspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvCspringApplication.class, args);
+	}
+
+}
---
+++ MVCspring/MVCspring/src/test/java/com/kiranAcademy/MVCspring/MvCspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiranAcademy.MVCspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvCspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
